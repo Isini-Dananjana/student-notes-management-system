3fadd1d76bb616501fb13592da5b91857f57b5e1
Isini-Dananjana <isinidan97@gmail.com>
2022-07-06 13:54:07 +0530

    added new functionalities to manage notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
-import Login from "./components/Login/login";
-import Signup from "./components/Signup/signup";
-import UserProfile from "./components/userProfile/userProfile";
-import EmailVerify from "./components/EmailVerify/emailverfiy";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
+import UserProfile from "./components/UserProfile/UserProfile";
+import EmailVerify from "./components/EmailVerify/EmailVerfiy";
+import CreateNote from "./components/Notes/CreateNote";
+import EditNote from "./components/Notes/EditNote";
+import AllNotes from "./components/Notes/AllNotes";
 import { Route, Routes, Navigate } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 function App() {
   return (
@@ -13,6 +17,9 @@ function App() {
 			<Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<Signup />} />
       <Route path="/profile" exact element={<UserProfile />} />
+      <Route path="/createNote" exact element={<CreateNote />} />
+      <Route path="/editNote/:id" exact element={<EditNote />} />
+      <Route path="/allNotes" exact element={<AllNotes />} />
       <Route path="/login/user/:id/verify/:token" element={<EmailVerify />} />
 		</Routes>
   );
---
+++ client/src/components/Notes/AllNotes.jsx
@@ -0,0 +1,200 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const ListNotes = () => {
+  const [allItems, setItem] = useState();
+  useEffect(() => {
+    const retrieveNotes = async () => {
+      try {
+        let result = await axios.get("http://localhost:8070/note/");
+        setItem(result.data);
+        console.log(result.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    retrieveNotes();
+  }, []);
+
+  return (
+    <div style={{ background: "#9A616D" }}> 
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a
+            className="navbar-brand"
+            style={{
+              color: "#9A616D",
+              padding: "5px",
+              fontWeight: "bold",
+              fontSize: "2rem",
+            }}
+            href="#"
+          >
+            NoteScope
+          </a>
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a
+                className="nav-link"
+                style={{
+                  color: "white",
+                  fontWeight: "bold",
+                }}
+                href="#"
+              >
+                Add Notes
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                style={{
+                  color: "white",
+                  fontWeight: "bold",
+                }}
+                href="#"
+              >
+                My Notes
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="row">
+        <section className="" style={{ background: "#9A616D" }}>
+          <div className="container ">
+            <div className="row d-flex">
+              <main role="main">
+                <section class="jumbotron text-center">
+                  <div class="container">
+                    <h1 class="jumbotron-heading">My Notes</h1>
+                    <p class="lead text-muted">
+                      Something short and leading about the collection below—its
+                      contents, the creator, etc. Make it short and sweet, but
+                      not too short so folks don't simply skip over it entirely.
+                    </p>
+                    <p>
+                      {" "}
+                      <Link
+                        to={"/createNote"}
+                        style={{
+                          textDecoration: "none",
+                          color: "#787b80",
+                        }}
+                      >
+                        <a href="#" class="btn btn-dark my-2">
+                          Add Notes
+                        </a>
+                      </Link>
+                      <a href="#" class="btn btn-secondary my-2">
+                        View Notes
+                      </a>
+                    </p>
+                  </div>
+                </section>
+                {allItems ? (
+                  allItems.map((item) => {
+                    return (
+                      <div
+                        style={{ width: "30%", float: "left", margin: "5px" }}
+                      >
+                        <div class="row">
+                          <div class="col s12 m7">
+                            <div
+                              class="card"
+                              style={{
+                                width: "24rem",
+                                padding: "3px",
+                              }}
+                            >
+                              <div
+                                className="card p-3 "
+                                style={{
+                                  width: "23rem",
+                                  color: "#9A616D",
+                                  borderWidth: "2px",
+                                  margin: "1px",
+                                  backgroundColor: "#f2f2f2",
+                                  padding: "1px",
+                                }}
+                              >
+                                <b>{item.title}</b>
+                              </div>
+                              <span
+                                class="card-title"
+                                style={{ color: "black" }}
+                              ></span>
+                              <div class="card-content">
+                                <p>{item.description}</p>
+                              </div>
+                              <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                  <button
+                                    type="button"
+                                    class="btn btn-sm btn-outline-secondary"
+                                  >
+                                    <Link
+                                      to={"/editNote/" + item._id}
+                                      style={{
+                                        textDecoration: "none",
+                                        color: "#787b80",
+                                      }}
+                                    >
+                                      Edit
+                                    </Link>
+                                  </button>
+                                  <button
+                                    type="button"
+                                    class="btn btn-sm btn-outline-secondary"
+                                    onClick={() => {
+                                      deleteItem(item._id);
+                                    }}
+                                  >
+                                    Delete
+                                  </button>
+                                </div>
+                                <small class="text-muted">9 mins</small>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })
+                ) : (
+                  <div>Data is loading</div>
+                )}
+              </main>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+let deleteItem = (id) => {
+  axios
+    .delete("http://localhost:8070/note/deleteNote/" + id)
+    .then((response) => {
+      console.log(response.data);
+      alert("deleted successfully")
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+export default ListNotes;
