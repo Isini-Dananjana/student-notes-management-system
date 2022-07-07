be34eb180b92097a51d9c049378784394fed0912
Isini-Dananjana <isinidan97@gmail.com>
2022-07-07 12:13:11 +0530

    added changes to userprofile.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
-
-import './App.css';
+import "./App.css";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
-import UserProfile from "./components/UserProfile/UserProfile";
+import UserProfile from "./components/userProfile/UserProfile";
 import EmailVerify from "./components/EmailVerify/Emailverfiy";
 import CreateNote from "./components/Notes/CreateNote";
 import EditNote from "./components/Notes/EditNote";
 import AllNotes from "./components/Notes/AllNotes";
 import AdminHome from "./components/Admin/home";
 import { Route, Routes, Navigate } from "react-router-dom";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-
-			<Route path="/login" exact element={<Login />} />
+      <Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<Signup />} />
       <Route path="/profile/:id" exact element={<UserProfile />} />
       <Route path="/createNote" exact element={<CreateNote />} />
@@ -23,7 +21,7 @@ function App() {
       <Route path="/allNotes" exact element={<AllNotes />} />
       <Route path="/admin" exact element={<AdminHome />} />
       <Route path="/login/user/:id/verify/:token" element={<EmailVerify />} />
-		</Routes>
+    </Routes>
   );
 }
 
