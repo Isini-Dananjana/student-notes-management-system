832af0a7aae2af441bca8e59a241464abf4d12a6
Isini Dananjana <66986207+Isini-Dananjana@users.noreply.github.com>
2022-07-07 18:41:02 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import UserProfile from "./components/userProfile/UserProfile";
-import EmailVerify from "./components/EmailVerify/Emailverfiy";
 import CreateNote from "./components/Notes/CreateNote";
 import EditNote from "./components/Notes/EditNote";
 import AllNotes from "./components/Notes/AllNotes";
@@ -20,7 +19,7 @@ function App() {
       <Route path="/editNote/:id" exact element={<EditNote />} />
       <Route path="/allNotes" exact element={<AllNotes />} />
       <Route path="/admin" exact element={<AdminHome />} />
-      <Route path="/login/user/:id/verify/:token" element={<EmailVerify />} />
+   
     </Routes>
   );
 }
